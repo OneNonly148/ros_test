52c8ed774cf11c9120a21f56a8bc7c489a62cec0
OneNonly148 <wanakashah14833@gmail.com>
2022-09-29 22:49:12 +0800

    Init git

---
+++ launch/talker.launch.py
@@ -0,0 +1,10 @@
+from launch import LaunchDescription, launchDescription
+from launch_ros.actions import Node
+
+def generate_launch_descriptions():
+    return LaunchDescription(
+        Node(
+            package='demo_nodes_cpp',
+            executable='talker'
+        )
+    )
